267f1e5c4b49da73238e57f55d17cb557d0b61de
Sachin Patney <spatney@microsoft.com>
2015-02-20 20:38:48 -0800

    changing compare to string

--- php/cam_cmd.php
+++ php/cam_cmd.php
@@ -1,6 +1,6 @@
 <?php
     $on = $_GET['on'];
-    if($on === 1){
+    if($on === '1'){
         $output = shell_exec("sudo raspimjpeg 2>&1");
     }else{
         $output = shell_exec("sudo pkill raspimjpeg");
