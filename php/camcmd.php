1975a6347700d045b085937eec67af8b2d4e256d
Sachin Patney <spatney@microsoft.com>
2015-02-20 19:38:17 -0800

    piping error thru

--- php/camcmd.php
+++ php/camcmd.php
@@ -1,7 +1,7 @@
 <?php
 $angle = $_GET['angle'];
 $sno = $_GET['sno'];
-//$output = shell_exec("sudo ../cFiles/servo $angle $sno");
-$output = shell_exec("../cFiles/servo $angle $sno");
+$output = shell_exec("sudo ../cFiles/servo $angle $sno 2>&1");
+//$output = shell_exec("../cFiles/servo $angle $sno 2>&1");
 echo "<pre>$output</pre>";
 ?>
