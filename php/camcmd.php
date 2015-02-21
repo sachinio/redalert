db5dd5afccc737f4c11a741b5cea8bd6d1e3ef62
Sachin Patney <spatney@microsoft.com>
2015-02-20 19:36:34 -0800

    test

--- php/camcmd.php
+++ php/camcmd.php
@@ -2,6 +2,6 @@
 $angle = $_GET['angle'];
 $sno = $_GET['sno'];
 //$output = shell_exec("sudo ../cFiles/servo $angle $sno");
-$output = shell_exec("whoami");
+$output = shell_exec("../cFiles/servo $angle $sno");
 echo "<pre>$output</pre>";
 ?>
