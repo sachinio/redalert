c23838609638099bc85ec0678f64bf6558535048
Sachin Patney <spatney@microsoft.com>
2015-02-20 19:44:27 -0800

    moving alert to output, removing stale code

--- php/camcmd.php
+++ php/camcmd.php
@@ -2,6 +2,5 @@
 $angle = $_GET['angle'];
 $sno = $_GET['sno'];
 $output = shell_exec("sudo ../cFiles/servo $angle $sno 2>&1");
-//$output = shell_exec("../cFiles/servo $angle $sno 2>&1");
-echo "<pre>$output</pre>";
+echo $output;
 ?>
