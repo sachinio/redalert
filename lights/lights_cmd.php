cfb05575966c4efb09d8dfaecb6379faf9137033
sachinio <sachin.patney@gmail.com>
2015-03-27 13:24:40 -0700

    fixing bugs

--- lights/lights_cmd.php
+++ lights/lights_cmd.php
@@ -5,5 +5,5 @@ $del = $_GET['del'];
 $bri = $_GET['bri'];
 $rgb = $_GET['rgb'];
 $tout = $_GET['tout'];
-$output = shell_exec(("sudo python lights.py $addr $cmd $del $bri $rgb $tout 2>&1");
+$output = shell_exec("sudo python lights.py $addr $cmd $del $bri $rgb $tout 2>&1");
 echo $output
