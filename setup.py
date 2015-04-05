47a2e5f359f21c69d772a05e70a4696a702fe655
Sachin Patney <sachin.patney@gmail.com>
2015-04-05 13:24:33 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,25 @@ print "Setting things up for you.. go grab a coffee or something!"
 
 time.sleep(2)
 
-#call(['sudo','apt-get','update']);
-#call(['sudo','apt-get','install','apache2', 'php5', 'libapache2-mod-php5','git','mpg123','-y'])
-#call(['sudo','apt-get','upgrade','-y'])
-os.chdir('camera')
+cw = os.getcwd();
+
+# Get latest
+call(['sudo','apt-get','update']);
+# Install required libraries
+call(['sudo','apt-get','install','apache2', 'php5', 'libapache2-mod-php5','git','mpg123','-y'])
+# Update OS/Libraries
+call(['sudo','apt-get','upgrade','-y'])
+os.chdir('/var/www')
+call('sudo mkdir git'.split(' '))
+os.chdir('git')
+call('git clone https://github.com/sachinio/redalert.git redalert'.split(' '))
+os.chdir('redalert/camera')
+print 'Compiling servo.c'
 call('sudo gcc -o servo servo.c -lwiringPi'.split(' '))
 os.chdir('../../')
 call('git clone https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code.git adafruit'.split(' '))
+
+call(['sudo','rm -rf', cw])
 
 # other thinsgs to automate
 # 1. add apache to sudo
