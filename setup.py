ff80a75bc95107b05b37bd882f1c18c8794fe2c7
Sachin Patney <sachin.patney@gmail.com>
2015-04-05 13:00:52 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@
 # frameworks/libraries
 
 from subprocess import call
+import os
+import time
 
-call(['sudo','apt-get','update']);
-call(['sudo','apt-get','install','apache2', 'php5', 'libapache2-mod-php5','git','mpg123','-y'])
-call(['sudo','apt-get','upgrade','-y'])
-call(['sudo','mkdir','../servoboard'])
+print "Setting things up for you.. go grab a coffee or something!"
+
+time.sleep(2)
+
+#call(['sudo','apt-get','update']);
+#call(['sudo','apt-get','install','apache2', 'php5', 'libapache2-mod-php5','git','mpg123','-y'])
+#call(['sudo','apt-get','upgrade','-y'])
+os.chdir('../')
+call(['sudo','mkdir','hello'])
