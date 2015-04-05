440f2c0b5463cb7065b881aae7fd2a78af552c83
Sachin Patney <sachin.patney@gmail.com>
2015-04-05 13:14:25 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 # This file will aid in installing all required
 # frameworks/libraries
 
+# IMPORTANT NOTE: Call this from the Redalert GIT root
+
 from subprocess import call
 import os
 import time
@@ -12,5 +14,12 @@ time.sleep(2)
 #call(['sudo','apt-get','update']);
 #call(['sudo','apt-get','install','apache2', 'php5', 'libapache2-mod-php5','git','mpg123','-y'])
 #call(['sudo','apt-get','upgrade','-y'])
-os.chdir('../')
+os.chdir('camera')
+call('sudo gcc -o servo servo.c -lwiringPi'.split(' '))
+os.chdir('../../')
 call('git clone https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code.git adafruit'.split(' '))
+
+# other thinsgs to automate
+# 1. add apache to sudo
+# 2. create temp fs for camera
+# 3. Add crone jobs for server
