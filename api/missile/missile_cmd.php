eff9c58e3d6f32f0d9d1dd1117343a96adf7ad17
sachinio <sachin.patney@gmail.com>
2015-04-15 21:58:50 -0700

    echoing missile cmd

--- api/missile/missile_cmd.php
+++ api/missile/missile_cmd.php
@@ -1,6 +1,7 @@
 <?php
   $cmd = $_GET['cmd'];
   $val = $_GET['val'];
-  exec("sudo python missile.py $cmd $val");
+  $output = shell_exec(""sudo python missile.py $cmd $val 2>&1");
+  echo $output
 
 
