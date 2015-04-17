3660bc0c108506c2df7ea07657d7b393d4417fa8
sachinio <sachin.patney@gmail.com>
2015-04-16 23:17:57 -0700

    adding upload script

--- utilities/upload.php
+++ utilities/upload.php
@@ -38,7 +38,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Sorry file ". basename( $_FILES["fileToUpload"]["name"]). " encountered an error uploading" ;
     }
 }
 ?>
