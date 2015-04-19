3186aca884885390a1a9cd82959849eebce1d459
sachinio <sachin.patney@gmail.com>
2015-04-19 12:27:52 -0700

    updating jobs

--- services/jokes.py
+++ services/jokes.py
@@ -4,5 +4,5 @@ from common import IMonaTask
 
 class Joker(IMonaTask):
     def __run__(self, time):
-        if time[1] == '15' or time[1] == '45':
+        if time[1] == '15' or time[1] == '30' or time[1] == '45':
             Mona.tellARandomJoke()
