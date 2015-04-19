8a7fc7a003250a262b605805db6ea37209491332
sachinio <sachin.patney@gmail.com>
2015-04-19 01:14:18 -0700

    updating jobs

--- services/stock.py
+++ services/stock.py
@@ -3,9 +3,11 @@ __author__ = 'sachinpatney'
 from common import IMonaJob
 from common import Mona
 
-import urllib2
+import urllib2, random
 
 template = "Microsoft stock closed at {0}, {1}, {2}."
+motivate = ['come on people we can do better!']
+praise = ['Great job guys!']
 
 class stockInfo(IMonaJob):
     def __run__(self, time, lock):
@@ -19,5 +21,10 @@ class stockInfo(IMonaJob):
 
         msg = template.format(result[1].replace('.',' point '), dir, result[2].replace('.',' point ').replace('-', ''))
         Mona.speak(msg)
+
+        if dir == 'up':
+            Mona.speak(random.choice(praise))
+        else:
+            Mona.speak(random.choice(motivate))
 
 stockInfo().__run__(0, 0)
