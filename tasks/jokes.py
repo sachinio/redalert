f39e2ebfb455111c1ea59ab702190515a4ad1ffc
sachinio <sachin.patney@gmail.com>
2015-04-19 18:52:16 -0700

    updating tasks

--- tasks/jokes.py
+++ tasks/jokes.py
@@ -7,6 +7,6 @@ import random
 
 class Joker(IMonaTask):
     def __run__(self, time):
-        if int(time[1]) % 15 == 0:
+        if int(time[0]) % 10 == 0 and time[1] == 0:
             jokes = ['newword.mp3', 'policechief.mp3', 'antimatte.mp3']
             Mona.play_sound(REPOSITORY_ROOT+'/resources/sounds/'+random.choice(jokes))
