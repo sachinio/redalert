b2b040bd7e934bc26be7a139c04139d31eef3ab7
sachinio <sachin.patney@gmail.com>
2015-04-20 12:03:15 -0700

    updating scripts

--- tasks/static/email_ip.py
+++ tasks/static/email_ip.py
@@ -1,34 +1,32 @@
 __author__ = 'sachinpatney'
 
-import sys
-import urllib2, time
+import sys, urllib2, time, argparse
 
 sys.path.append('/var/www/git/redalert/tasks')
 
 from common import EMail
 from subprocess import check_output
-import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-w", "--skipwifi", help="skip waiting for wifi to log on",
-                    action="store_true")
-parser.add_argument("-p", "--skippublicip", help="skip getting public ip",
-                    action="store_true")
+parser.add_argument("-w", "--wifi", help="wait for wifi to log on", action="store_true")
+parser.add_argument("-p", "--public_ip", help="include public ip", action="store_true")
 
 args = parser.parse_args()
 
-if not args.skipwifi:
-    time.sleep(10)  # Let WIFI Connect
+if args.wifi:
+    time.sleep(10)  # Wait for WIFI to connect
 
 ifconfig = check_output(["ifconfig"])
 
-public_ip = 'Could not be determined'
-if not args.skippublicip:
+public_ip = 'Not requested'
+if args.public_ip:
     try:
         public_ip = urllib2.urlopen('http://ip.42.pl/raw').read()
     except:
-        print 'Could not determine public IP, only sending ifconfig.'
+        public_ip = 'Could not determine Public IP'
 
-out = "ifconfig\n------------------------------\n\n {0}\n\nPublic IP: {1}".format(ifconfig, public_ip)
+out = "Hey, I just rebooted, and here is some info!" \
+      "\n\nifconfig\n------------------------------\n\n {0}\n\n" \
+      "Public IP: {1}".format(ifconfig, public_ip)
 
-EMail('Red Alert controller boot info', out).send()
+EMail('onBoot info', out).send()
