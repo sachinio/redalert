3ffc99e22af0ef295eef6f14abaa7aa507af2fd2
sachinio <sachin.patney@gmail.com>
2015-04-21 13:26:00 -0700

    updating scripts

--- utilities/upload.php
+++ utilities/upload.php
@@ -36,7 +36,8 @@ if ($uploadOk == 0) {
 $name = $_POST['name'];
 $title = $_POST['title'];
 $content = $_POST['content'];
-$output = shell_exec("sudo python addTimelineItem.py $name $title $content basename($_FILES["fileToUpload"]["name"]) 2>&1");
+$r = "" . basename($_FILES["fileToUpload"]["name"]);
+$output = shell_exec("sudo python addTimelineItem.py $name $title $content 2>&1");
 echo "<h1>Done!</h1><a href='../clients/timeline.html'>go back</a>";
 
 ?>
