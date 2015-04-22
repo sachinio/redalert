8458d8a7a105d8d7fd91080c2a8e42306bde7bb6
sachinio <sachin.patney@gmail.com>
2015-04-22 01:24:11 -0700

    updating script

--- tasks/stock.py
+++ tasks/stock.py
@@ -5,9 +5,9 @@ from common import Mona
 from common import Timeline
 import urllib2, random
 
-template = "Microsoft stock closed at {0}, {1}, {2}."
-motivate = ['come on people we can do better!', 'OK. Clearly you guys need to work harder.']
-praise = ['Great job guys!', 'Well done! Go treat yourself to some coffee.']
+template = "Microsoft stock closed at {0}, {1}, {2}.{3}"
+motivate = [' Come on people we can do better!', ' OK. Clearly you guys need to work harder.']
+praise = [' Great job guys!', ' Well done! Go treat yourself to some coffee.']
 
 class StockTicker(IMonaTask):
     def __run__(self, time):
@@ -20,9 +20,9 @@ class StockTicker(IMonaTask):
             if float(result[2]) > 0:
                 dir = 'up'
 
-            msg = template.format(result[1].replace('.',' point '), dir, result[2].replace('.',' point ').replace('-', ''))
+            msg = template.format(result[1].replace('.',' point '), dir, result[2].replace('.',' point ').replace('-', ''),'')
             Mona.speak(msg);
-            Timeline.add_item('Mona', 'Stock update', msg + ' ' + random.choice(motivate), '', 'fa-bar-chart', 'success')
+            Timeline.add_item('Mona', 'Stock update', template.format(result[1], dir, result[2], random.choice(motivate), '', 'fa-bar-chart', 'success')
 
             if dir == 'up':
                 Mona.speak(random.choice(praise))
