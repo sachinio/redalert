f64a07cc4bbeccf2c22c1923def72cc6cd58dc03
sachinio <sachin.patney@gmail.com>
2015-04-21 17:28:22 -0700

    adding get timeline

--- utilities/csv_to_json.py
+++ utilities/csv_to_json.py
@@ -1,7 +1,9 @@
 __author__ = 'sachinpatney'
 
 import json, sys
-from common import read_csv_as_list
 
 sys.path.append('/var/www/git/redalert/tasks')
+
+from common import read_csv_as_list
+
 print json.dumps(read_csv_as_list(sys.argv[1]))
--- utilities/get_timeline.php
+++ utilities/get_timeline.php
@@ -1,4 +1,5 @@
 <?php
 $f = '/var/www/tmp/timeline.csv'
 $output = shell_exec("sudo python csv_to_json.py $f 2>&1");
+echo $output
 ?>
