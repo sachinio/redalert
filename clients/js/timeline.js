934ccc417eb120dedf1ee14c9061e0300c7ecf93
sachinio <sachin.patney@gmail.com>
2015-04-22 00:48:17 -0700

    updating script

--- clients/js/timeline.js
+++ clients/js/timeline.js
@@ -2,9 +2,14 @@ var timelineApp = angular.module('timelineApp', []);
 
 timelineApp.controller('TimelineCtrl', function ($scope) {
 
-$.get('../utilities/get_timeline.php', function(d){
-    var evts = JSON.parse(d);
-    $scope.events = evts.reverse();
-    $scope.$apply()
-});
+    var refresh = function(){
+        $.get('../utilities/get_timeline.php', function(d){
+            var evts = JSON.parse(d);
+            $scope.events = evts.reverse();
+            $scope.$apply()
+        });
+    }
+
+    refresh();
+    setInterval(refresh, 2000);
 });
