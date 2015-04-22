55856798262d01b8ccbad6234b8cd4d85cbd5167
sachinio <sachin.patney@gmail.com>
2015-04-21 23:43:26 -0700

    updating script

--- tasks/utility.py
+++ tasks/utility.py
@@ -1,5 +1,8 @@
 __author__ = 'sachinpatney'
 
 # Just a test area for scripts
+from common import Mona
+from common import REPOSITORY_ROOT;
+import random
 jokes = ['newword.mp3', 'policechief.mp3', 'antimatte.mp3']
 Mona.play_sound(REPOSITORY_ROOT+'/resources/sounds/'+random.choice(jokes))
