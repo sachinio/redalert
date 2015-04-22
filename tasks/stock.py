0dca46fa3edab6154a8fe54924f0918551f678b0
sachinio <sachin.patney@gmail.com>
2015-04-22 01:09:33 -0700

    updating script

--- tasks/stock.py
+++ tasks/stock.py
@@ -2,7 +2,7 @@ __author__ = 'sachinpatney'
 
 from common import IMonaTask
 from common import Mona
-
+from common import Timeline
 import urllib2, random
 
 template = "Microsoft stock closed at {0}, {1}, {2}."
@@ -11,7 +11,7 @@ praise = ['Great job guys!', 'Well done! Go treat yourself to some coffee.']
 
 class StockTicker(IMonaTask):
     def __run__(self, time):
-        if time[0] == '13' and time[1] == '15':
+        if time[0] == '1' and time[1] == '15':
             result = urllib2.urlopen("http://finance.yahoo.com/d/quotes.csv?s=MSFT&f=spc1").read()
             result = result.strip().split(',')
 
@@ -21,7 +21,8 @@ class StockTicker(IMonaTask):
                 dir = 'up'
 
             msg = template.format(result[1].replace('.',' point '), dir, result[2].replace('.',' point ').replace('-', ''))
-            Mona.speak(msg)
+            Mona.speak(msg);
+            Timeline.add_item('Mona', 'Stock update', msg, '', 'fa-bar-chart', 'success')
 
             if dir == 'up':
                 Mona.speak(random.choice(praise))
