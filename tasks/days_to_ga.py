41e6341b20f4327202850de82d250c0b0cf4a16b
sachinio <sachin.patney@gmail.com>
2015-04-22 23:45:29 -0700

    updating script

--- tasks/days_to_ga.py
+++ tasks/days_to_ga.py
@@ -13,7 +13,7 @@ class DaysToGA(IMonaTask):
             d1 = datetime.date(2015,7,24)
             delta = d1 - d0
             print delta.days
-            Timeline.add_item('mona', '{0} days to GA'.format(str(delta.days)),
+            Timeline.add_item('mona', '{0} days to GA!'.format(str(delta.days)),
                               '',
                               'owl.gif', 'fa-bullhorn',
                               'info')
