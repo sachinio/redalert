111a016239874f6a27520945cfaa1711abfa12ab
sachinio <sachin.patney@gmail.com>
2015-04-23 22:02:48 -0700

    adding unit tests

--- tests/commonTests.py
+++ tests/commonTests.py
@@ -19,12 +19,18 @@ class TestCSVMethods(unittest.TestCase):
         self.assertEqual(d['iam'], 'happy')
 
     def test_read_write_list(self):
-        write_list_to_csv(['A', 'B', 'C'], [{'A': '20', 'B': 30, 'C': {'complex': 'iam'}}], CSV_TEST_FILE)
+        write_list_to_csv(
+            ['A', 'B', 'C'],
+            [
+                {'A': '20', 'B': 30, 'C': {'complex': 'iam'}},
+                {'A': '20', 'B': 30, 'C': ['complex', 'you', 'are']}
+            ], CSV_TEST_FILE)
         l = read_csv_as_list(CSV_TEST_FILE)
-        self.assertEqual(len(l), 1)
+        self.assertEqual(len(l), 2)
         self.assertEqual(l[0]['A'], '20')
         self.assertEqual(l[0]['B'], '30')
         self.assertEqual(ast.literal_eval(l[0]['C'])['complex'], 'iam')
+        self.assertEqual(ast.literal_eval(l[1]['C'])[1], 'you')
 
 if __name__ == '__main__':
     unittest.main()
