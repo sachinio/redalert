a7b7044d216ec97f5e8edef4a3649611fa1dfeff
sachinio <sachin.patney@gmail.com>
2015-05-01 10:29:19 +0800

    adding UI virtualization

--- clients/js/timeline.js
+++ clients/js/timeline.js
@@ -1,6 +1,7 @@
 var timelineApp = angular.module('timelineApp', []);
 
 timelineApp.controller('TimelineCtrl', function ($scope) {
+    $scope.total = 10;
 
     var refresh = function () {
         $.get('../utilities/get_timeline.php', function (d) {
@@ -9,6 +10,20 @@ timelineApp.controller('TimelineCtrl', function ($scope) {
             $scope.$apply()
         });
     }
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() > $(document).height()) {
+            console.log('loading more');
+            if($scope.events.length >= $scope.total + 10){
+                $scope.total += 10;
+            }
+            else if($scope.events.length >= $scope.total){
+                $scope.total = $scope.events.length;
+            }else{
+                console.log('ignoring')
+            }
+        }
+    });
 
     refresh();
     setInterval(refresh, 2000);
