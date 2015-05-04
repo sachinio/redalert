8b9152a15b3e67221c45a82dbc8cc896411022bb
sachinio <sachin.patney@gmail.com>
2015-05-04 16:00:21 +0800

    adding text degrees fahrenheit

--- tasks/weather.py
+++ tasks/weather.py
@@ -12,7 +12,7 @@ class WeatherTask(ITask):
         if time['hour'] == '12' and time['min'] == '00':
             w = Weather.get_weather('98043')
 
-            message = 'The weather right now at {0} is {1}, and the temperature is around {2}.' \
+            message = 'The weather right now at {0} is {1}, and the temperature is around {2} degrees fahrenheit.' \
                 .format(w['name'], w['weather'][0]['main'], w['main']['temp'])
 
             Timeline.add_item_from_bot('Weather Report', message, '', 'fa-cloud', 'info')
