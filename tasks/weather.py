7a073fe1bbd07d397d843b9ed761a6e7b9b0d249
sachinio <sachin.patney@gmail.com>
2015-05-04 16:02:20 +0800

    adding text degrees fahrenheit

--- tasks/weather.py
+++ tasks/weather.py
@@ -10,7 +10,7 @@ class WeatherTask(ITask):
 
     def __run__(self, time):
         if time['hour'] == '12' and time['min'] == '00':
-            w = Weather.get_weather('98043')
+            w = Weather.get_weather('98052')
 
             message = 'The weather right now at {0} is {1}, and the temperature is around {2} degrees fahrenheit.' \
                 .format(w['name'], w['weather'][0]['main'], w['main']['temp'])
