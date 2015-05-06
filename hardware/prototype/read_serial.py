4e7cb0beb9d204d4d430e1ad64db23bf8f0fc202
sachinio <sachin.patney@gmail.com>
2015-05-06 21:53:45 +0800

    update prototype

--- hardware/prototype/read_serial.py
+++ hardware/prototype/read_serial.py
@@ -15,7 +15,7 @@ while True:
             s += ser.read(bytesToRead)
         else:
             if len(s) > 0:
-                print('Hex string ...')
+                print('Hex string')
                 print(binascii.hexlify(s))
-            s = b''
-            time.sleep(300)
+                s = b''
+                time.sleep(300)
