568f960d64897f7413618714c40d9abad5cff188
sachinio <sachin.patney@gmail.com>
2015-05-06 21:28:48 +0800

    update prototype

--- hardware/prototype/read_serial.py
+++ hardware/prototype/read_serial.py
@@ -1,13 +1,19 @@
 __author__ = 'sachinpatney'
 
 import serial
+import time
 
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1.0)
 
+s = b''
+
 while True:
         bytesToRead = ser.inWaiting()
+
         if bytesToRead > 0:
-            s = ser.read(bytesToRead)
-            print('===== start ß=====')
-            print(s)
-            print('===== end =====')
+            s += ser.read(bytesToRead)
+        else:
+            if len(s) > 0:
+                print(s)
+            s = b''
+            time.sleep(1)
