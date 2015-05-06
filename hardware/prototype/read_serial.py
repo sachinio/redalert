ba6432c55b3cf52209bee1cdf7cef2601c95adcc
sachinio <sachin.patney@gmail.com>
2015-05-06 21:18:08 +0800

    update prototype

--- hardware/prototype/read_serial.py
+++ hardware/prototype/read_serial.py
@@ -5,12 +5,9 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1.0)
 
 while True:
-    try:
         bytesToRead = ser.inWaiting()
         if bytesToRead > 0:
             s = ser.read(bytesToRead)
             print('===== start ß=====')
             print(s)
             print('===== end =====')
-    except:
-        print('Exception .. timeout ?')
