cfce91465bc3445284f0523bb89e0ad29e3782f9
sachinio <sachin.patney@gmail.com>
2015-05-08 19:28:08 +0800

    update prototype

--- hardware/prototype/read_serial.py
+++ hardware/prototype/read_serial.py
@@ -26,7 +26,7 @@ while True:
 
                 if s[6:8] == b'90':
                     data = s[32:-2]
-                    data = binascii.unhexlify(data).decode('utf-8')
+                    data = binascii.unhexlify(data).decode('utf-8').strip()
                     print(data)
                     print(len(data))
                     do(data.strip())
