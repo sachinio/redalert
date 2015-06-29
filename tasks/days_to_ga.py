3f3bd9dc6ef6c771a2436ba206390c624394d131
sachinio <sachin.patney@gmail.com>
2015-06-29 01:32:25 -0700

    adding logging

--- tasks/days_to_ga.py
+++ tasks/days_to_ga.py
@@ -20,6 +20,7 @@ class DaysToGA(ITask):
             d0 = datetime.datetime.now().date()
             d1 = datetime.date(2015, 7, 24)
             delta = d1 - d0
+            print('{0} days to GA'.format(delta.days))
             Timeline.add_item_from_bot('{0} days to GA :)'.format(delta.days),
                                        '',
                                        pictures[delta.days % len(pictures)],
