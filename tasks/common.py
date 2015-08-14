4d687b836a4b5ceb376476ebcac8fe48f4473012
sachinio <sachin.patney@gmail.com>
2015-08-14 01:35:49 -0700

    github task

--- tasks/common.py
+++ tasks/common.py
@@ -322,7 +322,9 @@ class Timeline:
             urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
                               img)
             if len(urls) > 0:
-                img = urls[0].replace(')', '')
+                final = urls[0].replace(')', '')
+                img = final
+                print(img)
             else:
                 img = '{0}/{1}/{2}'.format('../../../uploads', name.lower(), img)
 
