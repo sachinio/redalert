7bf1215780f6043f4d9e88e31342c67e9e916c98
sachinio <sachin.patney@gmail.com>
2015-08-14 00:56:02 -0700

    github task

--- tasks/github.py
+++ tasks/github.py
@@ -3,6 +3,7 @@ import json
 import base64
 from common import safe_read_dictionary
 from common import sync_read_status_file
+from common import sync_write_to_status_file
 
 from urllib.request import urlopen
 from urllib.request import Request
@@ -10,12 +11,15 @@ from urllib.request import Request
 class Github(ITask):
     def __run__(self, time):
         info = self.get_build_info()
+        issues = []
         for i in info:
-            if safe_read_dictionary(i,'pull_request') is None:
+            if safe_read_dictionary(i, 'pull_request') is None:
                 print(i['id'])
                 print(i['title'])
                 print(i['user']['login'])
-
+                issues.append(i['id'])
+        self.writeReportedIssues(issues)
+        print(self.getReportedIssues())
 
     def get_auth(self):
         d = sync_read_status_file()
@@ -32,5 +36,14 @@ class Github(ITask):
         result = urlopen(request)
         response = result.read().decode('ascii')
         return json.loads(response)
+
+    def getReportedIssues(cls):
+        return safe_read_dictionary(sync_read_status_file(), 'issues').split()
+
+    def writeReportedIssues(cls, issues):
+        str = ''
+        for issue in issues:
+            str += issue + ' '
+        sync_write_to_status_file('issues', str)
 
 Github().__run__(None)
